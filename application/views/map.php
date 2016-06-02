4a8920154dbb6a88bcdfea548ec5678b686c06f0
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-03 00:29:51 +0100

    missing genus

--- application/views/map.php
+++ application/views/map.php
@@ -383,7 +383,7 @@
 								<optgroup label="Coral genera">	
 								<?php
 									foreach($genuses as $genus){
-										if ($genus != "") {
+										if ($genus != "" or $genus != "Not listed" or $genus != "Missing") {
 											echo "<option value='".$genus."'>".$genus."</option>";
 										}
 									}
