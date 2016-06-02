4667339e0e65884d55e5efcc98b10a0bc9c98467
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-02 13:51:13 +0100

    r

--- application/models/mapmodel.php
+++ application/models/mapmodel.php
@@ -62,11 +62,11 @@ class MapModel extends CI_Model {
     		{
     			//we retrieve the data from each fossil from each project
     			$query2 = $this->db->query('SELECT data_id, image_id, genus, species, age, country, place, collector FROM project_1_data'/*.' WHERE '.$where_string*/);
-
-                
+                return $query2->result_array();
+    /*
 				if($query2->num_rows>0){
 					array_merge($return, $query2->result_array());
-				}
+				}*/
     			
     		}
     	}
