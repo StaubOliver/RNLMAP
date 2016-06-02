b2a9a5447b18754e639fa9647d416f0b102bb337
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-02 23:20:57 +0100

    test view

--- application/controllers/map.php
+++ application/controllers/map.php
@@ -15,21 +15,21 @@ class App extends MY_Controller {
     
 	public function index() {		
 		// Check if logged in
-		if(!$this->ion_auth->logged_in()) {
+		//if(!$this->ion_auth->logged_in()) {
 			// Not logged in, show the home page
 			//$data['analytics'] = $this->load->view('analytics', NULL, TRUE);
-			$this->load->view('home', $data);
+		//	$this->load->view('home', $data);
 			
 			// Log the data
         	//$this->LoggerModel->logData();
-		} else {
+		//} else {
 			// Logged in show the app (Angular application)
-			$data['user_id'] = $this->ion_auth->get_user_id();
-			$data['projects'] = $this->ProjectModel->listProjects();
+			//$data['user_id'] = $this->ion_auth->get_user_id();
+			//$data['projects'] = $this->ProjectModel->listProjects();
 			//$data['analytics'] = $this->load->view('analytics', NULL, TRUE);
-			$data['is_admin'] = $this->ProfileModel->isAdmin();
+			//$data['is_admin'] = $this->ProfileModel->isAdmin();
 
-			$data['genuses'] = $this->MapMpdel->loadGenuses()
+			$data['genuses'] = $this->MapMpdel->loadGenuses();
 		
 			$this->load->view('map', $data);
 			
