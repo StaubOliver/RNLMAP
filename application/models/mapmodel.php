8a40247052bb319bac59eefc40ae12740ba74fdf
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-02 15:47:26 +0100

    test

--- application/models/mapmodel.php
+++ application/models/mapmodel.php
@@ -12,6 +12,53 @@ class MapModel extends CI_Model {
         parent::__construct();
     }
 
+    // function to geocode address, it will return false if unable to geocode address
+    function geocode($address){
+     
+        // url encode the address
+        $address = urlencode($address);
+         
+        // google map geocode api url
+        $url = "http://maps.google.com/maps/api/geocode/json?address={$address}";
+     
+        // get the json response
+        $resp_json = file_get_contents($url);
+         
+        // decode the json
+        $resp = json_decode($resp_json, true);
+     
+        // response status will be 'OK', if able to geocode given address 
+        if($resp['status']=='OK'){
+     
+            // get the important data
+            $lati = $resp['results'][0]['geometry']['location']['lat'];
+            $longi = $resp['results'][0]['geometry']['location']['lng'];
+            $formatted_address = $resp['results'][0]['formatted_address'];
+             
+            // verify if data is complete
+            if($lati && $longi && $formatted_address){
+             
+                // put the data in the array
+                $data_arr = array();            
+                 
+                array_push(
+                    $data_arr, 
+                        $lati, 
+                        $longi, 
+                        $formatted_address
+                    );
+                 
+                return $data_arr;
+                 
+            }else{
+                return false;
+            }
+             
+        }else{
+            return false;
+        }
+    }
+
     /**
     *   Load all fossils given the informations of genus, species, age and collector form every project
     */
@@ -66,8 +113,13 @@ class MapModel extends CI_Model {
                 //return $query2->result_array(); 
     
 				//if($query2->num_rows>0){
-			    $return[] = $query2->result_array();
+			    //$return[] = $query2->result_array();
 				//}
+
+                foreach ($query2->result_array() as $row)
+                {
+                    return $row;
+                }
     			
     		}
             //return the data
