8c1fba1835bccb99538043fb6b4fe89d227a5ce8
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-02 16:01:28 +0100

    testgeo

--- application/models/mapmodel.php
+++ application/models/mapmodel.php
@@ -119,9 +119,13 @@ class MapModel extends CI_Model {
                 foreach ($query2->result_array() as $row)
                 {
                     $temp = $this->geocode($row['country'].' '.$row['place']);
-                    $row['lattitude'] = $temp[0];
-                    $row['longitude'] = $temp[1];
-                    return $row;
+                    
+                    if ($temp != false) {
+                        $row['lattitude'] = $temp[0];
+                        $row['longitude'] = $temp[1];
+                    }
+                    
+                    $return[] = $row;  
 
                 }
     			
