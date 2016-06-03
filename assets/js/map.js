921b13f627821f488f0213be14970bad5a17ed25
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-03 12:08:33 +0100

    angular

--- assets/js/map.js
+++ assets/js/map.js
@@ -38,20 +38,7 @@ var filter = [];
 
 
 
-function refresh($http) {
-	deleteMarkers;
-	//retrieve the fossils and put them as marker in the map
-	$http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/-1/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
-		data.forEach(function(item, index){
-			var info = [];
-			info['lat'] = item['lattitude'];
-			info['lng'] = item['longitude'];
-			info['title'] = item['genus'];
-			createMarkers(info);	
-			console.log(info);
-		});
-	});
-}
+
 
 
 var map = angular.module('map', [])
@@ -67,8 +54,22 @@ var map = angular.module('map', [])
 
 	actualmap = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-	
-	refresh($http);
+	function refresh() {
+		deleteMarkers;
+		//retrieve the fossils and put them as marker in the map
+		$http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/-1/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
+			data.forEach(function(item, index){
+				var info = [];
+				info['lat'] = item['lattitude'];
+				info['lng'] = item['longitude'];
+				info['title'] = item['genus'];
+				createMarkers(info);	
+				console.log(info);
+			});
+		});
+	}
+
+	refresh();
 	
 
 });
