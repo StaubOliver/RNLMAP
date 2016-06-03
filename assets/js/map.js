48b54e642845449b4e7c13ecdf866a3855dcfc4a
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-03 12:10:51 +0100

    test

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,6 +2,11 @@ var actualmap;
 var markers = [];
 
 var filter = [];
+filter['project'] = "-1";
+filter['genus'] = "-1";
+filter['ageMin'] = "Quaternary";
+filter['ageMax'] = 'Precambrian';
+filter['collector'] = '-1';
 
 
 // Sets the map on all markers in the array.
@@ -54,47 +59,40 @@ var map = angular.module('map', [])
 
 	actualmap = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-	function refresh() {
-		deleteMarkers;
-		//retrieve the fossils and put them as marker in the map
-		$http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/-1/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
-			data.forEach(function(item, index){
-				var info = [];
-				info['lat'] = item['lattitude'];
-				info['lng'] = item['longitude'];
-				info['title'] = item['genus'];
-				createMarkers(info);	
-				console.log(info);
-			});
+	deleteMarkers;
+	//retrieve the fossils and put them as marker in the map
+	console.log('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/-1/-1/-1/-1/-1/-1');
+
+	$http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/-1/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
+		data.forEach(function(item, index){
+			var info = [];
+			info['lat'] = item['lattitude'];
+			info['lng'] = item['longitude'];
+			info['title'] = item['genus'];
+			createMarkers(info);	
+			console.log(info);
 		});
-	}
-
-	refresh();
+	});
 	
-
 });
 
 map.controller('filterSection', function($scope, $http){
 
-	filter['project'] = "-1";
-	filter['genus'] = "-1";
-	filter['ageMin'] = "Quaternary";
-	filter['ageMax'] = 'Precambrian';
-	filter['collector'] = '-1';
+	/*
 
 	$scope.selectedProject = filter['project'];
 	$scope.selectedGenus = filter['genus'];
 	$scope.selectedAgeMin = filter['ageMin'];
 	$scope.selectedAgeMax = filter['ageMax'];
 	$scope.selectedCollector = filter['collector'];
+	*/
 	
-	/*
 	$scope.selectedProject = "-1";
 	$scope.selectedGenus = "-1";
 	$scope.selectedAgeMin = "Quaternary";
 	$scope.selectedAgeMax = "Precambrian";
 	$scope.selectedCollector = "-1";
-*/
+
 	$scope.newGenus = function(){
 		console.log(filter['genus']);
 		filter['genus'] = $scope.selectedGenus;
