f7823a725b29d09d46b5ffcc91244a98a1492637
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-06 16:14:09 +0100

    test

--- assets/js/map.js
+++ assets/js/map.js
@@ -125,7 +125,7 @@ map.controller('filterSection', function($scope, $http){
 			});
 		});
 		$scope.nbfossils = markers.length;
-		console.log(markers.length);
+		console.log(markers.size);
 	}
 
 	$scope.newProject = function (){
