4e9f4fa8e322c1e122e1f8a21c3441d727e2b83c
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-06 16:40:38 +0100

    test

--- assets/js/map.js
+++ assets/js/map.js
@@ -109,7 +109,7 @@ map.controller('filterSection', function($scope, $http){
 	$scope.selectedAgeMax = "Precambrian";
 	$scope.selectedCollector = "-1";
 	*/
-	/*
+	
 	var refresh = function(){
 		deleteMarkers();
 		//retrieve the fossils and put them as marker in the map
@@ -128,31 +128,31 @@ map.controller('filterSection', function($scope, $http){
 				createMarkers(info);	
 			});
 		});
-	}*/
+	}
 
 	$scope.newProject = function (){
 		filter['project'] = $scope.selectedProject;
-		refresh($http);
+		refresh();
 	}
 
 	$scope.newGenus = function(){
 		filter['genus'] = $scope.selectedGenus;
-		refresh($http);
+		refresh();
 	}
 
 	$scope.newAgeMin = function(){
 		filter['ageMin'] = $scope.selectedAgeMin;
-		refresh($http);
+		refresh();
 	}
 
 	$scope.newAgeMax = function(){
 		filter['ageMax'] = $scope.selectedAgeMax;
-		refresh($http);
+		refresh();
 	}
 
 	$scope.newCollector = function(){
 		filter['collector'] = $scope.selectedCollector;
-		refresh($http);
+		refresh();
 	}
 
 });
